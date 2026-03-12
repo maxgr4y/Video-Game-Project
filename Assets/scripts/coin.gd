45f8325f08b81cdd9a25006d34d5c19bb056f626
maxgr4y <mgray@ftstudent.org>
2026-03-12 12:45:47 -0400

    it works

--- Assets/scripts/coin.gd
+++ Assets/scripts/coin.gd
@@ -1,18 +1,8 @@
 extends Area2D
 
-<<<<<<< Updated upstream
-
-
-
-
-func _on_body_entered(body: Node2D) -> void:
-	print("You got a Coin!")
-	queue_free()
-=======
 @onready var game_manager: Node = %"Game Manager"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
 	animation_player.play("pickup")
->>>>>>> Stashed changes
--- Assets/scripts/player.gd
+++ Assets/scripts/player.gd
@@ -4,10 +4,7 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-<<<<<<< Updated upstream
-=======
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
->>>>>>> Stashed changes
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,14 +12,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-<<<<<<< Updated upstream
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-=======
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -45,7 +34,6 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.play("jump")
 	
 	#apply movement
->>>>>>> Stashed changes
 	if direction:
 		velocity.x = direction * SPEED
 	else:
